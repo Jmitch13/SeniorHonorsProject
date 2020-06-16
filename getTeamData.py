e2e627db90db198c15c180deda2cbb8ae03d27f6
Joshua Mitchell <36055284+Jmitch13@users.noreply.github.com>
2020-06-16 09:22:53 -0700

    Update getTeamData.py

--- getTeamData.py
+++ getTeamData.py
@@ -5,13 +5,15 @@ from bs4 import BeautifulSoup as bs
 mystring = ""
 teamNames = ["NYY", "LAA", "LAD", "SDP", "DET", "BAL", "BOS", "TBD", "TOR", "CHW", "CLE", "KCR", "MIN", "HOU", "OAK", "SEA", "TEX", "ATL", "FLA", "NYM", "PHI", "WSN", "CHC", "CIN", "MIL", "PIT", "STL", "ARI", "COL", "SFG"]
 years = ["2005", "2006", "2007", "2008", "2009"]
-mylinks = []       
+
 for i in teamNames:
 	for j in years:
-                webpage = requests.get(f"https://www.baseball-reference.com/teams/{i}/{j}.shtml")
-                src = webpage.content
-                soup = bs(src, "html.parser")
-                x = soup.find("h1")
-                y = list(x.text)
-                z= ''.join(y)
-                print(z.replace("\n", " "))
+		webpage = requests.get(f"https://www.baseball-reference.com/teams/{i}/{j}.shtml")
+		src = webpage.content
+		soup = bs(src, "html.parser")
+		x = soup.find("h1")
+		z = ''.join(list(x.text))
+		print(z.replace("\n", " "))
+		a_tag = soup.find('tbody')
+		for k in a_tag.find_all('a'):
+			print(k.text)
